edbecf3f8137cb287078de110170096f6d1a890b
Che Carsner <checarsner@gmail.com>
2016-03-18 14:36:13 -0700

    Step-4

---
+++ app/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by checarsner on 3/15/16.
+ */
+
+angular.module('CalcApp', []);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['CalcApp']);
+});
+
+
+
